9ca3548d8f9d6daf76577298d401f095944a456f
JeongIn-Huh <66407729+jeongin98@users.noreply.github.com>
2023-08-22 12:42:26 +0900

    [unrated] Title: 원소들의 곱과 합, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181929. 원소들의 곱과 합/원소들의 곱과 합.js
+++ 프로그래머스/unrated/181929. 원소들의 곱과 합/원소들의 곱과 합.js
@@ -5,13 +5,12 @@ function solution(num_list) {
 
 // 모든 원소들 곱 함수
 const mul = (num_list) => {
-    return num_list.reduce((acc,cur) => acc * cur);
-               
+    return num_list.reduce((acc,cur) => acc * cur);      
 }
 // 합의 제곱 함수
 const sum = (num_list) => {
     let result = num_list.reduce((acc,cur) => acc + cur);
-    return result*result;
+    return result;
 }
 
 function compare(num_list){
@@ -19,8 +18,6 @@ function compare(num_list){
     const mulRes = mul(num_list);
     const sumRes = sum(num_list);
     
-    if(mulRes<sumRes) return 1;
-    
-    
-    return 0;
+    // 거듭제곱(**2)
+    return mulRes < sumRes**2 ? 1:0;
 }
