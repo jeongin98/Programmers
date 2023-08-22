19317e5b779f203942aed888704695783e6e866a
JeongIn-Huh <66407729+jeongin98@users.noreply.github.com>
2023-08-22 10:16:56 +0900

    [unrated] Title: 문자열 출력하기, Time: 64.52 ms, Memory: 32.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181952. 문자열 출력하기/문자열 출력하기.js
+++ 프로그래머스/unrated/181952. 문자열 출력하기/문자열 출력하기.js
@@ -2,13 +2,4 @@ const readline = require('readline');
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
-});
-
-let input = [];
-
-rl.on('line', function (line) {
-    input = [line];
-}).on('close',function(){
-    str = input[0];
-    console.log(str)
-});
+}).on('line', console.log)
