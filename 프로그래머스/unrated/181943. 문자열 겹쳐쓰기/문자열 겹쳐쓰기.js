5b2d0c7423bae387da95bcd134d96aecd4f13adc
JeongIn-Huh <66407729+jeongin98@users.noreply.github.com>
2023-08-23 14:26:56 +0900

    [unrated] Title: 문자열 겹쳐쓰기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.js
+++ 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.js
@@ -1,12 +1,7 @@
 function solution(my_string, overwrite_string, s) {
-
-    
     // 인덱스 s부터 my_string 길이까지 잡아
-    // 그 부분부터 replace
+    // 그 부분부터 slice
     // my_string에 result 갖다붙여
-    var str1 = my_string.slice(0,s)+overwrite_string;
-    var str2 = my_string.slice(str1.length,my_string.length+1)
-    console.log(str1+str2);
-   return str1+str2;
+   return my_string.slice(0,s)+overwrite_string+my_string.slice(s+overwrite_string.length);
    
 }
