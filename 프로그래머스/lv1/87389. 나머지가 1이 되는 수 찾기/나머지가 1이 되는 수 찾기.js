8a8fa1a6aeabd5860fd4dc8adc3f7199480049df
JeongIn-Huh <66407729+jeongin98@users.noreply.github.com>
2023-08-26 23:56:18 +0900

    [level 1] Title: 나머지가 1이 되는 수 찾기, Time: 4.26 ms, Memory: 36.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.js
@@ -0,0 +1,24 @@
+/*
+    #요구사항
+    n을 x로 나눈 나머지가 1이 되는지 확인하고, 그 중 min 값 찾기
+    
+    #구현
+    - 반복문 돌리며 if n%x가 1인지 확인
+        - 맞다면 min값과 비교
+*/
+
+function findMinDiv(n){
+    
+    var min = n;
+    
+    for(let i=2;i<n;i++){
+        if(n%i == 1 && i<min)
+            min = i;
+        }
+    
+    return min;
+}
+
+function solution(n) {
+   return findMinDiv(n);
+}
