00a0e2d614022b94708e351249a087d42dcae98e
JeongIn-Huh <66407729+jeongin98@users.noreply.github.com>
2023-09-08 10:27:12 +0900

    [level 2] Title: 숫자의 표현, Time: 1.28 ms, Memory: 36.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.js
@@ -0,0 +1,33 @@
+function solution(n) {
+    return count(n);
+}
+
+function count(n){
+    
+    let sum = 0;
+    let cnt = 0;
+    let j;
+    
+    for(let i=1;i<=n;i++){
+        j = i;
+        sum = 0;
+        while(sum<n){
+            sum+=i;
+            i++;
+        }
+        i=j;
+        
+        if(sum == n) 
+            cnt++;
+        
+    }
+    
+    return cnt;
+   
+}
+
+ 
+
+// i부터 연속한 수를 더하다가 15이상이 되면 판단 시작
+// sum이 15가 되면 cnt, 
+// sum이 15 넘어가는 순간 pass
