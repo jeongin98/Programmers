d692b36be09059c06246159e0a2231525a50c81d
JeongIn-Huh <66407729+jeongin98@users.noreply.github.com>
2023-09-15 14:15:00 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.03 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
+++ 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
@@ -1,23 +1,28 @@
-function repeatStr(n){
-    // 문자열 변수 str
-    const first = '수';
-    const second = '박';
-    
-    let str = "";
-    
-    // while n이 3까지
-    for(let i=1;i<=n;i++){
-    // n이 홀수면 push first 문자
-    // n이 홀수면 push second 문자
-        if(i%2==1)
-            str+=first;
-        else
-            str+=second;
-        }
-     return str;
-    }
-    
+/* 간단한 코드 */
+// repeat(2.5) → repeat(2)로 취급
+const solution = n => "수박".repeat(n/2+1).slice(0,n)
 
-function solution(n) {
-    return repeatStr(n);
-}
+
+
+//function repeatStr(n){ 
+//     // 문자열 변수 str
+//     const first = '수';
+//     const second = '박';
+    
+//     let str = "";
+    
+//     // while n이 3까지
+//     for(let i=1;i<=n;i++){
+//     // n이 홀수면 push first 문자
+//     // n이 홀수면 push second 문자
+//         if(i%2==1)
+//             str+=first;
+//         else
+//             str+=second;
+//         }
+//      return str;
+//     }
+    
+// function solution(n) {
+//     return repeatStr(n);
+// }
