f2edb4fc121931c5694475de05efd043982bb82a
JeongIn-Huh <66407729+jeongin98@users.noreply.github.com>
2023-09-21 23:46:42 +0900

    [unrated] Title: 크기가 작은 부분문자열, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.js
+++ 프로그래머스/unrated/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.js
@@ -1,10 +1,14 @@
 /*
-    # 기능구현
+    ## 기능구현
     - p 의 길이 저장
     - p의 길이만큼 t를 slice
     - slice한 문자열과 문자열 p를 숫자로 변환하여 비교(문자열 -> 숫자)
     - p보다 작거나 같을 경우 count
     - count 리턴
+    
+    ## 새로 배운 JS 문법
+    - Number말고 +를 붙이는 것만으로도 숫자로 형변환
+    ex. +p : p를 Number로 형변환
 */
 
 
@@ -16,7 +20,7 @@ function findCount(t, p){
     
     for(let i=0;i<=t.length-length_p;i++){
     slicedStr = t.slice(i,i+length_p ) // p의 길이만큼 slice(i부터 i+length_p 직전까지)
-    if(Number(slicedStr) <= Number(p)) cntResult++;
+    if(Number(slicedStr) <= Number(p)) cntResult++; 
     }
     
     return cntResult;
