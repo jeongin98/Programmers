55275a9f09becceb5d06694a50b99119568cf4ec
JeongIn-Huh <66407729+jeongin98@users.noreply.github.com>
2024-02-01 23:57:03 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.py
@@ -0,0 +1,8 @@
+def solution(numbers):
+    sum = 0
+    for item in range(1,10):
+        if item not in numbers:
+            sum += item
+    return sum
+
+# 다른 방법 : set으로 숫자 제외 후 sum(numbers에 없는 숫자) 하는 방법
