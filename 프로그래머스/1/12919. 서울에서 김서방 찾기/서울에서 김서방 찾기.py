771db3a2b91cb6176ca71dcdf3594513437114bd
JeongIn-Huh <66407729+jeongin98@users.noreply.github.com>
2024-02-04 11:22:37 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.py
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.py
@@ -1,8 +1,11 @@
 def solution(seoul):
     answer = ''
     
-    idx = seoul.index("Kim")
+#     idx = seoul.index("Kim")
     
-    answer = "김서방은 "+ str(idx) +"에 있다"
+#     answer = "김서방은 "+ str(idx) +"에 있다"
     
-    return answer
+#     return answer
+    return "김서방은 {}에 있다".format(seoul.index("Kim"))
+
+# 변수 idx를 쓰지 않아도 되는 방법
