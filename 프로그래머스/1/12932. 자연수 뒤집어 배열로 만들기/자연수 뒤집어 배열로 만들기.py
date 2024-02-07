bf0fb09b96a7a8a049746ea7b56bbf0a7e3fdb1e
JeongIn-Huh <66407729+jeongin98@users.noreply.github.com>
2024-02-07 22:57:21 +0900

    [level 1] Title: 자연수 뒤집어 배열로 만들기, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.py
+++ 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.py
@@ -1,4 +1,9 @@
+# [정수 n → 리스트] 변환 방법 :
+# 정수 → 문자열로 변환 → 리스트 컴프리헨션으로 각 문자로 쪼개기
 def solution(n):
-    answer = [int(digit) for digit in str(n) ]
-    answer.reverse() # reverse()는 원본을 뒤집고 None을 반환함
-    return answer
+    # answer = [int(digit) for digit in str(n) ] # 리스트 컴프리헨션
+    # answer.reverse() # reverse()는 원본을 뒤집고 None을 반환함
+    # return answer
+    
+    return list(map(int, reversed(str(n)))) # 다른 방법2
+
