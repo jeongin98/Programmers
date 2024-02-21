dc4c659ba93b02ae8e2e0f764df3fc8bb28138a1
JeongIn-Huh <66407729+jeongin98@users.noreply.github.com>
2024-02-21 11:15:41 +0900

    [Bronze I] Title: 쉽게 푸는 문제, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/1292. 쉽게 푸는 문제/쉽게 푸는 문제.py
+++ 백준/Bronze/1292. 쉽게 푸는 문제/쉽게 푸는 문제.py
@@ -1,15 +1,18 @@
-n,m = map(int, input().split())
+# 효율적인 코드로 변경
+
+A,B = map(int, input().split())
 
 arr = []
 num = 1
 
-for i in range(1,m+1):
-    for j in range(i):
+while len(arr) < B + 1:
+    for _ in range(num):
         arr.append(num)
     num += 1
 
-sum = 0
-for i in range(n-1,m):
-    sum += arr[i]
-   
-print(sum)
+# sum = 0
+# for i in range(n-1,m):
+#    sum += arr[i]
+# print(sum)
+
+print(sum(arr[A - 1:B])) # 한 줄로 범위 선택 + sum 가능
